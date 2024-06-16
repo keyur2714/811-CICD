43c02a9eface73077cb1fe842d7e245b3d30b97a
keyur2714 <keyurjava27@gmail.com>
2024-06-16 16:56:20 +0530

    updatd default api

--- src/main/java/com/dbdemo/demo/DemoApplication.java
+++ src/main/java/com/dbdemo/demo/DemoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello Radhe Krishna...!";
+	}
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
